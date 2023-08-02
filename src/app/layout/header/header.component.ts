cab7b9fb135debabe11ec8afc9f1db811609c9f4
Иван Сташко <4sv.exe@gmail.com>
2023-08-02 23:08:14 +0300

    account and heaer is complete

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component } from "@angular/core";
+import { UserService } from "src/app/shared/services/user.service";
+import { UserInterface } from './../../interfaces/index';
+import { User } from "src/app/db";
 
 @Component({
   selector: "app-header",
@@ -6,5 +9,16 @@ import { Component } from "@angular/core";
   styleUrls: ["./header.component.scss"],
 })
 export class HeaderComponent {
-  
+  readonly IMG_AVATARS_PATH = "assets/images/pages/account/avatars/";
+
+  user: UserInterface = User;
+
+  constructor(private userService: UserService) {
+    this.user.name = this.userService.getName();
+    this.user.avatar = this.userService.getAvatar();
+    this.user.combo = this.userService.getCombo();
+    // this.user.faction = this.userService.getFaction();
+    // this.user.dateOfLogin = this.userService.getDateOfLogin();
+    // this.user.selectedTechnologies = this.userService.getSelectedTechnologies();
+  }
 }
--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -1,7 +1,7 @@
 import { Component } from "@angular/core";
 import { UserService } from "src/app/shared/services/user.service";
 import { UserInterface } from "./../../interfaces/";
-import { User } from "src/app/db";
+import { User, UserAvatars } from "src/app/db";
 
 @Component({
   selector: "app-account",
@@ -12,8 +12,9 @@ export class AccountComponent {
   readonly IMG_AVATARS_PATH = "assets/images/pages/account/avatars/";
 
   user: UserInterface = User;
+  userAvatars: string[] = UserAvatars;
 
-  isOpenAvatarsBox = true;
+  isOpenAvatarsBox = false;
 
   constructor(private userService: UserService) {
     this.user.name = this.userService.getName();
@@ -23,7 +24,15 @@ export class AccountComponent {
     this.user.selectedTechnologies = this.userService.getSelectedTechnologies();
   }
 
-  changeAvatar(){
+  changeAvatar(avatar: string) {
+    this.userService.setAvatar(avatar);
+    this.user.avatar = this.userService.getAvatar();
+  }
 
+  setUserName(name: string) { 
+    if (name.length !== 0) {
+      this.userService.setName(name);
+      this.user.name = this.userService.getName();
+    }
   }
 }
