6b29909b560ff983479f3e2f834be5bcb687cd89
Иван Сташко <4sv.exe@gmail.com>
2023-08-02 16:38:55 +0300

    create user servuce & user page

--- src/app/pages/account/account.component.ts
+++ src/app/pages/account/account.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { UserService } from "src/app/shared/services/user.service";
+import { UserInterface } from "./../../interfaces/";
+import { User } from "src/app/db";
 
 @Component({
-  selector: 'app-account',
-  templateUrl: './account.component.html',
-  styleUrls: ['./account.component.scss']
+  selector: "app-account",
+  templateUrl: "./account.component.html",
+  styleUrls: ["./account.component.scss"],
 })
 export class AccountComponent {
+  readonly IMG_AVATARS_PATH = "assets/images/pages/account/avatars/";
 
+  user: UserInterface = User;
+
+  constructor(private userService: UserService) {
+    this.user.name = this.userService.getName();
+    this.user.faction = this.userService.getFaction();
+    this.user.avatar = this.userService.getAvatar();
+    this.user.dateOfLogin = this.userService.getDateOfLogin();
+    this.user.selectedTechnologies = this.userService.getSelectedTechnologies();
+  }
 }
