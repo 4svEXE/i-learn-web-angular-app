81e0a8a0a097984ecaf160acde1a77e7f4ed0ed3
Иван Сташко <4sv.exe@gmail.com>
2023-08-05 16:37:35 +0300

    add preTest page

--- src/app/pages/preparation-for-the-test/preparation-for-the-test.component.ts
+++ src/app/pages/preparation-for-the-test/preparation-for-the-test.component.ts
@@ -1,10 +1,41 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Technologies } from "src/app/db/Tecnologies";
+import { TechnologiesService } from "./../../shared/services/technologies.service";
+import { RecomendetFractionsTechnologies } from "./../../db/RecomendetFractionsTechnologies";
+import { UserService } from './../../shared/services/user.service';
 
 @Component({
-  selector: 'app-preparation-for-the-test',
-  templateUrl: './preparation-for-the-test.component.html',
-  styleUrls: ['./preparation-for-the-test.component.scss']
+  selector: "app-preparation-for-the-test",
+  templateUrl: "./preparation-for-the-test.component.html",
+  styleUrls: ["./preparation-for-the-test.component.scss"],
 })
 export class PreparationForTheTestComponent {
+  readonly IMG_PATH: string = "";
 
+  technologies = Technologies;
+  sortedTechnologies!: string[];
+
+  selectedTech!: string[]; // from local storage
+  recomendetTech = RecomendetFractionsTechnologies;
+
+  userFraction!: string;
+
+  constructor(private techService: TechnologiesService, private userService: UserService) {
+    this.selectedTech = this.techService.getSelectedTech();
+
+    // Set user fraction
+    this.userFraction = userService.getFraction()
+
+    // Set tech
+    this.sortedTechnologies = this.recomendetTech[this.userFraction];
+  }
+
+  renderListOfTec(tech: string) {
+    if (tech === "Recomendet") {
+
+    }
+    if (tech === "All") {
+      this.sortedTechnologies = this.technologies
+    }
+  }
 }
